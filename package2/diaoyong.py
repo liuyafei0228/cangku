0d7c3257c507ecbdad390391a285db9d44a378fc
liuyafei0228 <1349612386@qq.com>
2019-12-20 14:53:14 +0800

    测试

---
+++ package2/diaoyong.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from package1.package1class1 import phone
+
+o = phone("黑")
+o.chicun()
+o.duanxin()
---
+++ py1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#f = open("D:\\software\\pycharm\\liuyafeipy\\xinjian21.txt","a",encoding='utf-8')
+#f.write("nishisha")
+#f.writelines(['1231\n','456\n','我是'])
+#f.close()
+
+#f  = open("D:\\software/pycharm\\liuyafeipy\\xinjian21.txt","r",encoding="utf-8")
+#d = f.read()
+#print(d)
+#lines = f.readlines()
+#print(lines)
+#f.close()
+#with open("D:\\software/pycharm\\liuyafeipy\\xinjian21.txt","r",encoding="utf-8") as f:
+#    c = f.read()
+ #   print(c)
+
+a = open("D:\\software\\pycharm\\liuyafeipy\\xinjian05.txt","a")
+a.write("123456")
+a.close()
+
+
